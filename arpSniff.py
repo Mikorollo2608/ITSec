6a4809d91df6d44dd29f7e01c27543c42b3e0df4
Mikorollo2608 <michal.p2608@wp.pl>
2024-05-04 13:32:43 -0400

    Small changes

--- arpSniff.py
+++ arpSniff.py
@@ -4,11 +4,19 @@ import time
 
 
 def get_mac(ip):
-    arp_request = ARP(pdst=ip)
-    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
-    return answered_list[0][1].hwsrc
+    mac = "xx"
+    while mac == "xx":
+        try:
+            arp_request = ARP(pdst=ip)
+            broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+            arp_request_broadcast = broadcast / arp_request
+            answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
+            mac = answered_list[0][1].hwsrc
+        except:
+            print("No mac")
+            pass
+        finally:
+            return mac
 
 
 def spoof(t_ip, spoof_ip):
